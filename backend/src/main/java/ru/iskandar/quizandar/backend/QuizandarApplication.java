36e60e77cf542d12a71236d9d205f6826a4e63eb
Iskandar684 <Iskandar684@gmail.com>
2026-06-21 23:07:31 +0300

    Добавлена структура backend, класс запуска приложения.

---
+++ backend/src/main/java/ru/iskandar/quizandar/backend/QuizandarApplication.java
@@ -0,0 +1,13 @@
+package ru.iskandar.quizandar.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizandarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizandarApplication.class, args);
+	}
+
+}
